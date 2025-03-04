9ea0df6beda0648050926b2b6cbd2b712d8b9307
guanyc <guanyc@gmail.com>
2025-03-04 08:17:26 +0000

    copilot sencod commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  // member.js
+  var member = {
+    name: 'John Doe',
+    skills: ['JavaScript', 'React', 'Node']
+  };
+  return member;
+}
+
+module.exports = skillsMember;
